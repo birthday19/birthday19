86ee2076133fc99706b8a26c527dcd7cc9e7464a
birthday19 <87622829+birthday19@users.noreply.github.com>
2021-07-19 04:47:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+   var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Nishtha Jain" && b=="20-07-2002")
+  	{
+  	    window.location.href="Birthday.html";
+  	}
+  	else
+  	{
+  		alert("Try Again");
+  		window.location.href="index.html";
+  	}
+  	
